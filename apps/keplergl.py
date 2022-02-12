4d1e0a73698c6095889a0142e6ab5959bc2ee726
ursha <benas.pekarskis@gmail.com>
2022-02-12 17:58:49 +0000

    aded gdf

--- apps/keplergl.py
+++ apps/keplergl.py
@@ -9,7 +9,8 @@ def app():
     st.title("keplergl")
 
     m = leafmap.Map(center=[50, -2], zoom=5)
-    gdf = gpd.read_file('/home/ursha/Documents/kepler.gl/Hex_GlocationFull_UK.geojson')
+    gdf = gpd.read_file("https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/world_cities.geojson")
+
     m.add_gdf(gdf, layer_name="Countries")
     m.to_streamlit(height=700)
 
